678d2d5acc301af873264940ecab0f2c5d08922a
Luis Silva <luisfilipersilva@gmail.com>
2015-08-28 18:46:35 -0300

    Create upload.php
    code in php to upload works. set in action, called in upload.js

---
+++ upload.php
@@ -0,0 +1,21 @@
+$targetFolder = '/var/path_to_files'; // Relative to the root
+
+	$verifyToken = md5('unique_salt' . $_POST['timestamp']);
+
+	if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
+		$tempFile = $_FILES['Filedata']['tmp_name'];
+		//$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+		$targetPath = $targetFolder;
+		$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
+		
+		// Validate the file type
+		$fileTypes = array('jpg','jpeg','gif','png','csv'); // File extensions
+		$fileParts = pathinfo($_FILES['Filedata']['name']);
+		
+		if (in_array($fileParts['extension'],$fileTypes)) {
+			move_uploaded_file($tempFile,$targetFile);
+			echo '1';
+		} else {
+			echo 'Invalid file type.';
+		}
+	}
